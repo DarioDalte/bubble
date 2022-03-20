dd7daeee33423c648e21c815987553693c6226fc
DarioDalte <dariodalte@gmail.com>
2022-03-20 19:57:17 +0100

    beta best seller

--- components/Main/BestSeller/BestSeller.js
+++ components/Main/BestSeller/BestSeller.js
@@ -6,7 +6,7 @@ function BestSeller() {
     <div className={classes.container}>
       <span className={classes.text}>Best Seller</span>
       <div className={classes["best-seller"]}>
-        <div className={classes['title-container']}>
+        <div className={classes["title-container"]}>
           <span className={classes.title}>Cuffie</span>
           <span className={classes.subtitle}>Compra ora</span>
         </div>
@@ -19,11 +19,41 @@ function BestSeller() {
           ></Image>
         </div>
       </div>
-      <div className={classes["bottom-container"]}>
-        <div className={classes["products"]}>
-          <div className={classes["product"]}>test</div>
-          <div className={classes["product"]}>test</div>
-          <div className={classes["product"]}>test</div>
+
+      <div className={classes["scrolling-wrapper"]}>
+        <div className={classes.card}>
+          <Image
+            src="/headphone.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          ></Image>
+          <span className={classes.title}>Cuffie</span>
+          <span className={classes.price}>€ 50</span>
+          <span className={classes.review}>Recensioni</span>
+        </div>
+        <div className={classes.card}>
+          <Image
+            src="/headphone.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          ></Image>
+          <span className={classes.title}>Cuffie</span>
+          <span className={classes.price}>€ 50</span>
+          <span className={classes.review}>Recensioni</span>
+        </div>
+        <div className={classes.card}>
+          {" "}
+          <Image
+            src="/headphone.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          ></Image>
+          <span className={classes.title}>Cuffie</span>
+          <span className={classes.price}>€ 50</span>
+          <span className={classes.review}>Recensioni</span>
         </div>
       </div>
     </div>
