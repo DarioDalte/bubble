a20c469b85c74de265a2b5354e7cebe778102ac2
Arslanx844 <arslanshaukat844@gmail.com>
2022-03-31 20:14:01 +0200

    Update [...nextauth].js
    aggiungi il lower case che ti ho detto

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { hashPassowrd, verifyPassword } from "../middlewares/auth.js";
-
 const databaseConnection = require("../middlewares/database.js");
+
 const client = await databaseConnection();
 
 export default NextAuth({
@@ -15,7 +15,6 @@ export default NextAuth({
         await client.connect();
 
         const usersCollection = client.db().collection("users");
-
         const user = await usersCollection.findOne({
           email: credentials.email,
         });
