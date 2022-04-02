eddfe4e7c637f2f17614f33f3825a97b7c39b414
DarioDalte <dariodalte@gmail.com>
2022-04-02 09:58:14 +0200

    comments

--- pages/api/middlewares/database.js
+++ pages/api/middlewares/database.js
@@ -1,12 +1,15 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";  //To connect to a MongoDB database
 
 module.exports = async function () {
-  const dbName = "marketplace";
-  const dbPassword = "0W2iRdCWo1qqZqg8";
+  const dbName = "marketplace"; //DB name
+  const dbPassword = "0W2iRdCWo1qqZqg8";  //DB password
 
+  /**
+  * Connection URI
+  */
   const uri = `mongodb+srv://${dbName}:${dbPassword}@cluster0.j9ybu.mongodb.net/marketplace?retryWrites=true&w=majority`;
 
-  const client = new MongoClient(uri);
+  const client = new MongoClient(uri);  //create istance of MongoClient
 
 
   return client;
