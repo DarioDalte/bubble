6a5970a6a5496ed225651e01a7a05fe848e99cbc
Arslanx844 <arslanshaukat844@gmail.com>
2022-04-11 20:26:55 +0200

    Ricerca finita muah voto diesci

--- pages/api/ricerca.js
+++ pages/api/ricerca.js
@@ -17,83 +17,70 @@ export default async function handler(req, res) {
     const prodotti = await collection.find().toArray(); //Inserisco nella collection
 
     collection = db.collection("companies"); //Seleziono la collection
-    const brand = await collection.find().toArray(); //Inserisco nella collection
+    const companies = await collection.find().toArray(); //Inserisco nella collection
 
+    var stringa_1 = [];
 
-    console.log(brand);
-
-    function capitalize(str) {
-      const lower = str.toLowerCase();
-      return str.charAt(0).toUpperCase() + lower.slice(1);
-    }
-
-    data.stringa = capitalize(data.stringa);
-
+    data.stringa = data.stringa.toLowerCase();
+    data.stringa = data.stringa.split(" ").join("");
+    stringa_1.push("RICERCA" + data.stringa + "\n");
 
     var prodotti_ricerca = [];
     var myJSON = "";
-    var stringa_1 = [];
-    var stringa_2 = "";
-    var stringa_3 = "";
-    var stringa_4 = "";
-
-    var stringa_4 = "";
-
 
     for (var i = 0; i < prodotti.length; i++) {
       var nome_prodotto = prodotti[i]["name"];
+      nome_prodotto = nome_prodotto.split(" ").join("");
+      nome_prodotto = nome_prodotto.toLowerCase();
       myJSON = JSON.stringify(prodotti[i]);
-      stringa_1.push("prodotto selezionato: " + myJSON + "\n");
-
+      stringa_1.push("PRODOTTO: " + myJSON + "\n");
 
       if (nome_prodotto.includes(data.stringa)) {
-        prodotti_ricerca.push(nome_prodotto);
-        stringa_1.push("prodotto  aggiunto all'array: " + myJSON + "\n");
-
-
+        prodotti_ricerca.push(prodotti[i]["name"]);
+        stringa_1.push("RICERCA PRESENTE NEL NOME DEL PRODOTTO: " + "\n");
       } else {
-        let id_brand = prodotti[i]["brand"];  
-        stringa_1.push("brand prodotto: " + id_brand + "\n");
+        let id_brand = prodotti[i]["brand"];
+        stringa_1.push("ECCO L'ID DEL BRAND DEL PRODOTTO: " + id_brand + "\n");
 
+        for (var x = 0; x < companies.length; x++) {
+          myJSON = JSON.stringify(companies[x]);
+          stringa_1.push("COMPANY: " + myJSON + "\n");
+          var brand = companies[x]["_id"]
+            .toString()
+            .replace(/ObjectId\("(.*)"\)/, "$1");
 
+          if (brand === String(id_brand)) {
+            console.log("siiii è uguale");
+            var nome = String(companies[x]["name"]);
+            nome = nome.toLocaleLowerCase();
+            if (nome.includes(data.stringa)) {
+              console.log("siiii è uguale1");
+              stringa_1.push(
+                "PRODOTTO CONTENTE LA  RICERCA NEL NOME DEL BRAND" + "\n"
+              );
 
-        
-        for (var x = 0; x < brand.length; x++) {
-        
-
-          myJSON = JSON.stringify(brand[x]);
-          stringa_1.push("brand: " + myJSON);
-
-
-          if (brand[x]["_id"] == id_brand) {
-            if (brand[x]["name"].includes(data.stringa)) {
-              stringa_1.push("prodotto aggiunto ricerca" + "\n");
-          
-
-              prodotti_ricerca.push(nome_prodotto);
+              prodotti_ricerca.push(prodotti[i]["name"]);
             }
           }
         }
       }
     }
 
-    console.log(prodotti_ricerca);
-    
     stringa_1 = stringa_1.toString();
-  
-    const fs = require('fs')
 
-    
+    const fs = require("fs");
+
     try {
-      fs.writeFileSync('C:/Users/navfl/OneDrive/Desktop/Projects/bubble/pages/api/test.txt', stringa_1)
+      fs.writeFileSync(
+        "C:/Users/Arslan/OneDrive/Desktop/bubble/pages/api/test.txt",
+        stringa_1
+      );
       //file written successfully
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-    
 
-
-    res.json({ message: "funzia!" });
+    res.json({ prodotti: prodotti_ricerca });
   } catch (err) {
     //... handle it locally
     console.log(err.message);
