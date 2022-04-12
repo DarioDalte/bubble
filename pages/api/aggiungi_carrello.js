030ec9a48a0cbb6b112399211a4e708f2740e413
Arslanx844 <arslanshaukat844@gmail.com>
2022-04-12 12:41:21 +0200

    prova

---
+++ pages/api/aggiungi_carrello.js
@@ -0,0 +1,35 @@
+const databaseConnection = require("./middlewares/database.js");
+
+export default async function handler(req, res) {
+  const client = await databaseConnection();
+
+  try {
+    /**
+     *   The await operator is used to wait for a Promise
+     */
+    await client.connect(); //To connect to our cluster
+
+    let data = req.body; //Get data from HTTP request
+
+    const db = client.db(); //Boh
+
+    const collection = db.collection("cart"); //Seleziono la collection
+    var myquery = { email: data.email };
+    var newvalues = {
+      $set: {
+        prodotti: {
+          prodotto_1: data.prodotto,
+        },
+      },
+    };
+    await collection.updateOne(myquery, newvalues);
+    console.log("info inserite");
+    res.json({ message: "funzia!" });
+  } catch (err) {
+    //... handle it locally
+    console.log(err.message);
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await client.close();
+  }
+}
