e8568731ef7467704e2151c7d2eb9f545bd2d166
Arslanx844 <arslanshaukat844@gmail.com>
2022-04-17 21:23:01 +0200

    recensioni

--- pages/api/ricerca.js
+++ pages/api/ricerca.js
@@ -51,11 +51,11 @@ export default async function handler(req, res) {
       myJSON = JSON.stringify(prodotti[i]);
 
       if (nome_prodotto.includes(data.stringa)) {
-        var brand = await db
-          .collection("companies")
-          .findOne({ _id: prodotti[i]["brand"] });
+        // var brand = await db
+        //  .collection("companies")
+        //  .findOne({ _id: prodotti[i]["brand"] });
         //Selects documents f
-        prodotti[i]["brand"] = brand["name"];
+        //prodotti[i]["brand"] = brand["name"];
         prodotti_ricerca.push(prodotti[i]);
       } else {
         let id_brand = prodotti[i]["brand"];
@@ -70,7 +70,7 @@ export default async function handler(req, res) {
             var nome = String(companies[x]["name"]);
             nome = nome.toLocaleLowerCase();
             if (nome.includes(data.stringa)) {
-              prodotti[i]["brand"] = companies[x]["name"];
+              //prodotti[i]["brand"] = companies[x]["name"];
               prodotti_ricerca.push(prodotti[i]);
             }
           }
@@ -83,11 +83,11 @@ export default async function handler(req, res) {
       var boh = result[i]["category"].toLowerCase();
       if (boh.includes(data.stringa)) {
         for (var z = 0; z < result[i]["orderdetails"].length; z++) {
-          var brand = await db
-            .collection("companies")
-            .findOne({ _id: result[i]["orderdetails"][z]["brand "] });
+          //var brand = await db
+          //  .collection("companies")
+          //  .findOne({ _id: result[i]["orderdetails"][z]["brand "] });
           //Selects documents f
-          result[i]["orderdetails"][z]["brand"] = brand["name"];
+          //result[i]["orderdetails"][z]["brand"] = brand["name"];
           prodotti_ricerca.push(result[i]["orderdetails"][z]);
           a = 1;
         }
@@ -114,11 +114,11 @@ export default async function handler(req, res) {
                       .replace(/ObjectId\("(.*)"\)/, "$1");
 
                     if (variabile[r] == values[f]) {
-                      var brand = await db.collection("companies").findOne({
-                        _id: result[i]["orderdetails"][z]["brand"],
-                      });
+                      //var brand = await db.collection("companies").findOne({
+                      //  _id: result[i]["orderdetails"][z]["brand"],
+                      //});
                       //Selects documents f
-                      result[i]["orderdetails"][z]["brand"] = brand["name"];
+                      //result[i]["orderdetails"][z]["brand"] = brand["name"];
                       prodotti_ricerca.push(result[i]["orderdetails"][z]);
                     }
                   }
@@ -130,7 +130,69 @@ export default async function handler(req, res) {
       }
     }
 
-    res.json({ prodotti: prodotti_ricerca });
+    for (var i = 0; i < prodotti_ricerca.length; i++) {
+      delete prodotti_ricerca[i].category;
+      delete prodotti_ricerca[i].varianti;
+      delete prodotti_ricerca[i].pollici;
+      delete prodotti_ricerca[i].processore;
+      delete prodotti_ricerca[i].sub_categories;
+      delete prodotti_ricerca[i].OS;
+    }
+
+    var cart = [];
+    var recensioni = await db.collection("reviews").find().toArray(); //Selects documents from collection reviews
+    var elenco_recensioni = []; //Declare and initialize elenco_recensioni
+    var obj_recensioni = {}; //Declare and initialize obj_recensioni
+
+    for (var i = 0; i < recensioni.length; i++) {
+      let id = recensioni[i]["id_product"]
+        .toString()
+        .replace(/ObjectId\("(.*)"\)/, "$1");
+      obj_recensioni = {
+        id_prodotti: id,
+        value: recensioni[i]["value"],
+      };
+      elenco_recensioni.push(obj_recensioni);
+    }
+    let oggetto = {};
+    var cart = [];
+
+    for (var x = 0; x < prodotti_ricerca.length; x++) {
+      let id_prodotto = prodotti_ricerca[x]["_id"]
+        .toString()
+        .replace(/ObjectId\("(.*)"\)/, "$1");
+
+      var somma_recensioni = 0;
+      var cont = 0;
+      for (var b = 0; b < elenco_recensioni.length; b++) {
+        var singola_recensione = elenco_recensioni[b];
+        var id_prodotto_recensito = singola_recensione["id_prodotti"];
+        if (id_prodotto == id_prodotto_recensito) {
+          somma_recensioni = somma_recensioni + singola_recensione["value"];
+          cont++;
+        }
+      }
+      var media = somma_recensioni / cont;
+      console.log(prodotti_ricerca[x]["brand"]);
+      let yourId = mongoose.Types.ObjectId(prodotti_ricerca[x]["brand"]);
+
+      var brand = await db
+        .collection("companies")
+        .find({ _id: yourId })
+        .toArray();
+      //Selects documents from collection products
+      console.log("arrivo");
+
+      prodotti_ricerca[x]["brand"] = brand[0]["name"];
+      oggetto = {
+        prodotto: prodotti_ricerca[x],
+
+        star: media ? media : 0,
+      };
+      cart.push(oggetto);
+    }
+
+    res.json({ prodotti: cart });
   } catch (err) {
     //... handle it locally
     console.log(err.message);
