525d92cbf57594ac031aff6271e474ab76d06414
Arslanx844 <arslanshaukat844@gmail.com>
2022-04-18 21:28:02 +0200

    recensioni

---
+++ pages/api/elimina_recensione.js
@@ -0,0 +1,34 @@
+import { Logger } from "sass";
+const mongoose = require("mongoose");
+
+const databaseConnection = require("./middlewares/database.js");
+
+export default async function handler(req, res) {
+  const client = await databaseConnection(); //Mi connetto al db
+
+  try {
+    await client.connect();
+    const db = client.db(); //Boh
+    var data = req.body; //Inserts the request data into the variable data
+    var user = await db.collection("users").findOne({
+      email: data.email,
+    });
+    console.log(user["_id"]);
+    let yourId = mongoose.Types.ObjectId(data.id_product);
+    console.log(yourId);
+    const result = await db
+      .collection("reviews")
+      .deleteOne({ id_user: user["_id"], id_product: yourId });
+
+    if (result["deletedCount"] != 0) {
+      res.status(200).json({ message: "Recensione eliminata" });
+      return;
+    } else {
+      res.status(200).json({ message: "Ops.. qualcosa è andato storto" });
+      return;
+    }
+  } catch (e) {
+    //error
+    console.log("Error " + e);
+  }
+}
---
+++ pages/api/inserisci_recensione.js
@@ -0,0 +1,45 @@
+import { Logger } from "sass";
+const mongoose = require("mongoose");
+
+const databaseConnection = require("./middlewares/database.js");
+
+export default async function handler(req, res) {
+  const client = await databaseConnection(); //Mi connetto al db
+
+  try {
+    await client.connect();
+    const db = client.db(); //Boh
+    var data = req.body; //Inserts the request data into the variable data
+    var user = await db.collection("users").findOne({
+      email: data.email,
+    });
+    console.log(user["_id"]);
+    let yourId = mongoose.Types.ObjectId(data.id_product);
+    console.log(yourId);
+    var collection = db.collection("reviews"); //Seleziono la collection
+    var reviews = await collection.findOne({
+      id_product: yourId,
+      id_user: user["_id"],
+    });
+    //Inserisco nella collection
+    console.log(reviews);
+
+    if (!reviews) {
+      var obj = {
+        id_user: user["_id"],
+        text: data.text,
+        value: data.value,
+        id_product: yourId,
+      };
+      await db.collection("reviews").insertOne(obj);
+      res.status(200).json({ message: "Recensione aggiunta" });
+      return;
+    }
+
+    res.status(200).json({ message: "Prodotto già recensito" });
+    return;
+  } catch (e) {
+    //error
+    console.log("Error " + e);
+  }
+}
