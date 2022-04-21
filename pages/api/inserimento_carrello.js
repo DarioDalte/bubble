de1d31af27992c73a6fb5534d67bff48851aae76
DarioDalte <dariodalte@gmail.com>
2022-04-21 12:51:03 +0200

    inserimetno_carrello

--- pages/api/inserimento_carrello.js
+++ pages/api/inserimento_carrello.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
     var cart = await db.collection("cart").findOne({ email: data.email }); //Selects documents from collection product
     var prova = [];
-
+    var a = 0;
     var entra = [];
     var array = [];
     if (data.variant) {
@@ -32,7 +32,9 @@ export default async function handler(req, res) {
             String(cart.products[product][key]) ==
             String(mongoose.Types.ObjectId(data.id))
           ) {
-            console.log("cciao");
+            a = 1;
+          }
+          if (a == 1) {
             Object.keys(cart.products[product][key]).map((key_1, index) => {
               console.log("2222222222222222222222222222222");
               console.log(cart.products[product][key][key_1]);
